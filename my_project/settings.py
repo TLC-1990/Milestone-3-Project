22fafb17c45ab6556dac236761168533c688f85e
Thomas Le Coyte <TomLeCoyte@MacBook-Pro.local>
2025-08-19 16:12:17 +0100

    models defined for reservation information and process

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "booking_system_app"
+    "booking_system_app",
     "restaurant_info",
 ]
 

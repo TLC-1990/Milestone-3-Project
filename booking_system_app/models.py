78b9e8a5e3c64603b194c753c782eece77d90e63
Thomas Le Coyte <TomLeCoyte@MacBook-Pro.local>
2025-08-19 16:21:28 +0100

    Add STATIC_ROOT for Heroku deployment

--- booking_system_app/models.py
+++ booking_system_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    requested_time = models.DateTimeField()
+    other_notes = models.TextField(blank=True)
+    
+    def __str__(self):
+      return f"{self.name} - {self.requested_time}"
--- booking_system_app/views.py
+++ booking_system_app/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the booking system!")
+
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)

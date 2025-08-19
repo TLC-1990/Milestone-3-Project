b89dc11c9efaf19db5807d0979ff9b7fe33cbddf
Thomas Le Coyte <TomLeCoyte@MacBook-Pro.local>
2025-08-19 21:49:40 +0100

    Add models to chose available times for booking

--- booking_system_app/models.py
+++ booking_system_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import ForeignKey
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 from datetime import datetime, timedelta
@@ -11,10 +12,49 @@ class Reservation(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     requested_time = models.DateTimeField()
+    available_hour = models.ForeignKey(
+      'AvailableHour', 
+      on_delete=models.CASCADE,
+      related_name="reservations"
+    )
     other_notes = models.TextField(blank=True)
     
-    def __str__(self):
-      return f"{self.name} - {self.requested_time}"
   
+    DAYS_OF_WEEK = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+     ) 
+    
+class WeekDay(models.Model):
+    restaurant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="weekdays"
+    )
+    day = models.IntegerField(choices=Reservation.DAYS_OF_WEEK)
+       
+     # timeslot
+class AvailableHour(models.Model): #Based on weekday
+    weekday = models.ForeignKey(
+        WeekDay,
+        on_delete=models.CASCADE,
+        related_name="available_hours"
+    )
+    from_hour = models.TimeField()
+    to_hour = models.TimeField()
+    booked_by = ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 
-        
+def is_slot_available(available_hour, date):
+    existing = Reservation.objects.filter(
+        requested_time__date=date,
+        available_hour=available_hour
+    ).exists()
+    return not existing
+  
+    def __str__(self):
+      return f"{self.name} - {self.requested_time}"        

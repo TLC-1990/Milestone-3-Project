9296a90808254ebdcf7c4b213b075161cc21188c
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-08-25 16:53:02 +0100

    remove django.contrib.sites from installed_Apps as preventing login.html from loading

--- my_project/settings.py
+++ my_project/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.sites",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",

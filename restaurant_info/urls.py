f9eafc331ec6041d1faed65853c97e97d1d6474a
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-16 10:40:31 +0100

    remove superfluous users.app and restaurant_info.app. Fix issue with images not loading in menu

--- restaurant_info/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('menus/', views.menus_view, name='menus'),
-]
--- restaurant_info/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def menus_view(request):
-    return render(request, 'restaurant_info/menus.html')

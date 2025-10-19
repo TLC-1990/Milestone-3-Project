4eaec938bbf48228cb57ca6a22a065cd8dbe8db8
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-19 12:11:54 +0100

    resolve login templates and remove unecessary accounts app

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CustomLoginView, SignUpView
-urlpatterns = [
-path("login/", CustomLoginView.as_view(), name="login"),
-path('signup/', SignUpView.as_view(), name='signup'),
-]

af911673218895a75d9d815d82ff3530f63a211b
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-23 09:41:33 +0100

    update email settings so verification can occur outside of terminal

--- my_project/settings.py
+++ my_project/settings.py
@@ -154,5 +154,12 @@ BOOKING_BG = "img/booking_bg.jpg"
 BOOKING_SUCCESS_REDIRECT_URL = "reservation/success/"
 BOOKING_DISABLE_URL = "reservation/disabled/"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 DEFAULT_FROM_EMAIL = 'thewurstoftimes@example.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
